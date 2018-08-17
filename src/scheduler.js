26f82f172f082fb9f26c7fd177c6bfd2cfc6af9e
Szymon Mich <Szymon_Mich@mckinsey.com>
2018-08-17 17:54:48 +0200

    fetch in progress

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,10 +1,10 @@
-export const calcPP = (start, end, PPmin) => {
+const calcPP = (start, end, PPmin) => {
     let calcStart = new Date(start)
     let calcEnd = new Date(end)
     return ((calcEnd - calcStart) / 60000) * PPmin
 };
 
-export const PPAvalAfter = (avalArr, start) => {
+const PPAvalAfter = (avalArr, start) => {
     let result = avalArr.filter(e => {
             return !((new Date(e.End)) < (new Date(start)))
         })
@@ -17,7 +17,7 @@ export const PPAvalAfter = (avalArr, start) => {
     return result
 }
 
-export const PPUsedAfter = (jobs, start) => {
+const PPUsedAfter = (jobs, start) => {
     let result = jobs.filter(e => {
             return new Date(e.EndDate) > new Date(start)
         })
@@ -30,26 +30,31 @@ export const PPUsedAfter = (jobs, start) => {
     return result
 }
 
-export const PPAvalBetween = (avalArr, start, end) => {
+const PPAvalBetween = (avalArr, start, end) => {
     let result = avalArr.filter(e => {
-        return (new Date(e.End) > new Date(start))&&(new Date(e.Start) < new Date(end))
-    })
-    .reduce((acc, curr) => {
-        let startTime = (new Date(curr.Start))>(new Date(start)) ? new Date(curr.Start) : new Date(start)
-        let endTime = (new Date(curr.End))>(new Date(end)) ? new Date(end) : new Date(curr.End)
-        return acc += calcPP(startTime, endTime, curr.PPmin)
-        }, 0)
-    return result
-}
-
-export const PPUsedBetween = (jobs, start, end) => {
-    let result = jobs.filter(e => {
-            return (new Date(e.EndDate) > new Date(start))&&(new Date(e.StartDate) < new Date(end))
+            return (new Date(e.End) > new Date(start)) && (new Date(e.Start) < new Date(end))
         })
         .reduce((acc, curr) => {
-            let startTime = (new Date(curr.StartDate))>(new Date(start)) ? new Date(curr.StartDate) : new Date(start)
-            let endTime = (new Date(curr.EndDate))>(new Date(end)) ? new Date(end) : new Date(curr.EndDate)
+            let startTime = (new Date(curr.Start)) > (new Date(start)) ? new Date(curr.Start) : new Date(start)
+            let endTime = (new Date(curr.End)) > (new Date(end)) ? new Date(end) : new Date(curr.End)
             return acc += calcPP(startTime, endTime, curr.PPmin)
         }, 0)
     return result
 }
+
+const PPUsedBetween = (jobs, start, end) => {
+    let result = jobs.filter(e => {
+            return (new Date(e.EndDate) > new Date(start)) && (new Date(e.StartDate) < new Date(end))
+        })
+        .reduce((acc, curr) => {
+            let startTime = (new Date(curr.StartDate)) > (new Date(start)) ? new Date(curr.StartDate) : new Date(start)
+            let endTime = (new Date(curr.EndDate)) > (new Date(end)) ? new Date(end) : new Date(curr.EndDate)
+            return acc += calcPP(startTime, endTime, curr.PPmin)
+        }, 0)
+    return result
+}
+module.exports.PPUsedBetween = PPUsedBetween
+module.exports.PPAvalBetween = PPAvalBetween
+module.exports.PPAvalAfter = PPAvalAfter
+module.exports.PPUsedAfter = PPUsedAfter
+module.exports.calcPP = calcPP
--- test/scheduler.spec.js
+++ test/scheduler.spec.js
@@ -1,124 +1,117 @@
-import * as scheduler from '../../src/scheduler.js'
+const { calcPP, PPAvalBetween, PPUsedBetween, PPAvalAfter, PPUsedAfter } = require('../src/scheduler.js')
 
 test('Calculate fun', () => {
     let Start = "2018-08-06T10:00:00";
-    let End ="2018-08-06T11:00:00";
+    let End = "2018-08-06T11:00:00";
     let PPmin = 0.1;
-  expect(scheduler.calcPP(Start, End, PPmin)).toBe(6)
+    expect(calcPP(Start, End, PPmin)).toBe(6)
 });
 
 test('Calculate avalible PP full', () => {
-  let slots = [{
-    "Start":"2018-08-06T10:00:00",
-    "End":"2018-08-06T11:00:00",
-    "PPmin":0.1
-    }
-  ]
-  let start = '2018-08-06T10:00:00'
-  expect(scheduler.PPAvalAfter(slots, start)).toBe(6)
+    let slots = [{
+        "Start": "2018-08-06T10:00:00",
+        "End": "2018-08-06T11:00:00",
+        "PPmin": 0.1
+    }]
+    let start = '2018-08-06T10:00:00'
+    expect(PPAvalAfter(slots, start)).toBe(6)
 });
 
 test('Calculate avalible PP none', () => {
-  let slots = [{
-    "Start":"2018-08-06T10:00:00",
-    "End":"2018-08-06T11:00:00",
-    "PPmin":0.1
-    }
-  ]
-  let start = '2018-08-06T11:00:00'
-  expect(scheduler.PPAvalAfter(slots, start)).toBe(0)
+    let slots = [{
+        "Start": "2018-08-06T10:00:00",
+        "End": "2018-08-06T11:00:00",
+        "PPmin": 0.1
+    }]
+    let start = '2018-08-06T11:00:00'
+    expect(PPAvalAfter(slots, start)).toBe(0)
 });
 
 test('Calculate avalible PP partial before', () => {
-  let slots = [{
-    "Start":"2018-08-06T10:00:00",
-    "End":"2018-08-06T11:00:00",
-    "PPmin":0.1
-    }
-  ]
-  let start = '2018-08-06T10:30:00'
-  expect(scheduler.PPAvalAfter(slots, start)).toBe(3)
+    let slots = [{
+        "Start": "2018-08-06T10:00:00",
+        "End": "2018-08-06T11:00:00",
+        "PPmin": 0.1
+    }]
+    let start = '2018-08-06T10:30:00'
+    expect(PPAvalAfter(slots, start)).toBe(3)
 });
 
 test('Calculate avalible PP partial after', () => {
-  let slots = [{
-    "Start":"2018-08-06T10:00:00",
-    "End":"2018-08-06T11:00:00",
-    "PPmin":0.1
-    }
-  ]
-  let start = '2018-08-06T07:30:00'
-  expect(scheduler.PPAvalAfter(slots, start)).toBe(6)
+    let slots = [{
+        "Start": "2018-08-06T10:00:00",
+        "End": "2018-08-06T11:00:00",
+        "PPmin": 0.1
+    }]
+    let start = '2018-08-06T07:30:00'
+    expect(PPAvalAfter(slots, start)).toBe(6)
 });
 
 test('Calculate avalible PP', () => {
-  let jobs = [{
-    "StartDate": "2018-08-01 10:00",
-    "EndDate": "2018-08-01 11:00",
-    "PPmin":0.1
-    }
-  ]
-  let start = '2018-08-01T10:00:00'
-  expect(scheduler.PPUsedAfter(jobs, start)).toBe(6)
+    let jobs = [{
+        "StartDate": "2018-08-01 10:00",
+        "EndDate": "2018-08-01 11:00",
+        "PPmin": 0.1
+    }]
+    let start = '2018-08-01T10:00:00'
+    expect(PPUsedAfter(jobs, start)).toBe(6)
 });
 
 test('Calculate avalible PP partial before', () => {
-  let jobs = [{
-    "StartDate": "2018-08-01 10:00",
-    "EndDate": "2018-08-01 11:00",
-    "PPmin":0.1
-    }
-  ]
-  let start = '2018-08-01T10:30:00'
-  expect(scheduler.PPUsedAfter(jobs, start)).toBe(3)
+    let jobs = [{
+        "StartDate": "2018-08-01 10:00",
+        "EndDate": "2018-08-01 11:00",
+        "PPmin": 0.1
+    }]
+    let start = '2018-08-01T10:30:00'
+    expect(PPUsedAfter(jobs, start)).toBe(3)
 });
 
 test('Calculate avalible PP partial after', () => {
-  let jobs = [{
-    "StartDate": "2018-08-01 10:00",
-    "EndDate": "2018-08-01 11:00",
-    "PPmin":0.1
-    }
-  ]
-  let start = '2018-08-01T07:30:00'
-  expect(scheduler.PPUsedAfter(jobs, start)).toBe(6)
+    let jobs = [{
+        "StartDate": "2018-08-01 10:00",
+        "EndDate": "2018-08-01 11:00",
+        "PPmin": 0.1
+    }]
+    let start = '2018-08-01T07:30:00'
+    expect(PPUsedAfter(jobs, start)).toBe(6)
 });
 
 test('Calculate used PP partial', () => {
-  let jobs = [{
-    "StartDate": "2018-08-01 10:00",
-    "EndDate": "2018-08-01 11:00",
-    "PPmin":0.1
-    },
-    {
-      "StartDate": "2018-08-01 10:30",
-      "EndDate": "2018-08-01 11:00",
-      "PPmin":0.1
-      },
-    {
-      "StartDate": "2018-08-01 15:30",
-      "EndDate": "2018-08-01 16:00",
-      "PPmin":0.1
-      }
-  ]
-  let start = '2018-08-01T10:00:00'
-  let end = '2018-08-01T11:00:00'
-  expect(scheduler.PPUsedBetween(jobs, start, end)).toBe(9)
+    let jobs = [{
+            "StartDate": "2018-08-01 10:00",
+            "EndDate": "2018-08-01 11:00",
+            "PPmin": 0.1
+        },
+        {
+            "StartDate": "2018-08-01 10:30",
+            "EndDate": "2018-08-01 11:00",
+            "PPmin": 0.1
+        },
+        {
+            "StartDate": "2018-08-01 15:30",
+            "EndDate": "2018-08-01 16:00",
+            "PPmin": 0.1
+        }
+    ]
+    let start = '2018-08-01T10:00:00'
+    let end = '2018-08-01T11:00:00'
+    expect(PPUsedBetween(jobs, start, end)).toBe(9)
 });
 
 test('Calculate avalible PP between', () => {
-  let slots = [{
-    "Start":"2018-08-06T10:00:00",
-    "End":"2018-08-06T11:00:00",
-    "PPmin":0.1
-    },
-    {
-      "Start":"2018-08-06T10:50:00",
-      "End":"2018-08-06T11:00:00",
-      "PPmin":1
-      }
-  ]
-  let start = '2018-08-06T10:00:00'
-  let end = '2018-08-06T11:00:00'
-  expect(scheduler.PPAvalBetween(slots, start, end)).toBe(16)
+    let slots = [{
+            "Start": "2018-08-06T10:00:00",
+            "End": "2018-08-06T11:00:00",
+            "PPmin": 0.1
+        },
+        {
+            "Start": "2018-08-06T10:50:00",
+            "End": "2018-08-06T11:00:00",
+            "PPmin": 1
+        }
+    ]
+    let start = '2018-08-06T10:00:00'
+    let end = '2018-08-06T11:00:00'
+    expect(PPAvalBetween(slots, start, end)).toBe(16)
 });
